870ec24ce6ef4d5889efa721e13196aa035c3ef0
jansenicus <jansenicus@yahoo.com>
2018-05-29 16:13:33 +0700

    First Commit

---
+++ day1.py
@@ -0,0 +1,20 @@
+# _*_ utf-8
+
+def main():
+
+    x = input()
+
+    def valid(x):
+        if len(x) < = 10000
+            v = True
+        else:
+            v = False
+
+    while True:
+
+        v = valid(x):
+        if v: 
+            print('Hello Techgig\n'=x)
+            break
+        else:
+            x = input()
